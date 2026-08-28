fe404d92339eac64ef20f08e7ac6f61606b2e637
Mario Lazzari <mario.lazzari@gmail.com>
2026-08-28 09:30:46 +0200

    Done lesson 10

--- 01-intro/deck/src/main.rs
+++ 01-intro/deck/src/main.rs
@@ -4,7 +4,22 @@ struct Deck {
 }
 
 fn main() {
-    let deck = Deck { cards: vec![] };
+    // vars are unmutable by default
+    let suits = ["Hearts", "Spades", "Diamonds"];
+    let values = ["Ace", "Two", "Three"];
 
-    println!("My deck: {:?}", deck);
+    // mutable var
+    let mut cards = vec![];
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+
+    let deck = Deck { cards };
+
+    // formatted output with #
+    println!("My deck: {:#?}", deck);
 }
