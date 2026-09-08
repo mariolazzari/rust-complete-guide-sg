f01ca941ef5424e28839af7f16e9c0a66ce0e07c
Mario Lazzari <mario.lazzari@gmail.com>
2026-09-08 08:16:20 +0200

    Done lesson 44

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -13,6 +13,20 @@ impl Account {
             holder,
         }
     }
+
+    fn summary(&self) -> String {
+        format!("{} has a balance of {}", self.holder, self.balance)
+    }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
 }
 
 #[derive(Debug)]
@@ -28,12 +42,29 @@ impl Bank {
     fn add_account(&mut self, account: Account) {
         self.accounts.push(account);
     }
+
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+    }
 }
 
 fn main() {
     let mut bank = Bank::new();
-    let account = Account::new(1, String::from("Mario"));
+
+    let mut account = Account::new(1, String::from("Mario"));
+    account.deposit(500);
+    account.withdraw(250);
+    print!("{}", account.summary());
 
     bank.add_account(account);
-    println!("{:#?}", bank);
+
+    println!("{:#?}", bank.summary());
+    println!("{}", bank.total_balance());
 }
