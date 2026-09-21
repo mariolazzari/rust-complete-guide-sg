d80d400d9408a883cf7a3ab78885d5a2bad6837d
Mario Lazzari <mario.lazzari@gmail.com>
2026-09-21 08:30:02 +0200

    Done lesson 76

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,20 +1,29 @@
 use std::fs;
 
-fn string_test(a: String, b: &String, c: &str) {}
+fn extract_errors(text: &str) -> Vec<&str> {
+    let split_text = text.split('\n');
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line);
+        }
+    }
+
+    results
+}
 
 fn main() {
-    string_test(
-        "red".to_string(),
-        &String::from("red"),
-        String::from("red").as_str(),
-    );
+    let mut error_logs = vec![];
 
     match fs::read_to_string("logs.txt") {
         Ok(was_read) => {
-            println!("{:#?}", was_read.len());
+            error_logs = extract_errors(was_read.as_str());
         }
         Err(err) => {
             println!("error: {}", err)
         }
     }
+
+    println!("{:#?}", error_logs);
 }
